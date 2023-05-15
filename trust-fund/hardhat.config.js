5da78ed086eb65fbd408ea2aa409d611c264c055
Jeb <commit@jeb4.dev>
2023-05-15 17:22:28 +0300

    goerli -> sepolia
    Co-authored-by: Aminos24 <105971064+aminos24@users.noreply.github.com>

--- trust-fund/hardhat.config.js
+++ trust-fund/hardhat.config.js
@@ -2,19 +2,19 @@
 module.exports = {
     solidity: {
         version: '0.8.9',
-        defaultNetwork: 'goerli',
+        defaultNetwork: 'sepolia',
         networks: {
             hardhat: {},
-            goerli: {
+            sepolia: {
                 url: 'https://rpc.ankr.com/eth_sepolia',
-                accounts: [`0x${process.env.PRIVATE_KEY}`],
+                accounts: [`0x${process.env.PRIVATE_KEY}`]
             }
         },
         settings: {
             optimizer: {
                 enabled: true,
-                runs: 200,
-            },
-        },
-    },
+                runs: 200
+            }
+        }
+    }
 };

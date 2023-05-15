1257c7e6b1f0a0fbe33a771189308ff88125c672
Jeb <commit@jeb4.dev>
2023-05-15 18:11:40 +0300

    third time is the charm (frontend structure)
    Co-authored-by: Aminos24 <105971064+aminos24@users.noreply.github.com>

--- client/README.md
+++ client/README.md
@@ -3,12 +3,13 @@
 Create a project using this example:
 
 ```bash
-npx thirdweb create --template next-javascript-starter
+npx thirdweb create --template vite-javascript-starter
 ```
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+You can start editing the page by modifying `src/main.jsx`. The page auto-updates as you edit the file.
 
-On `pages/_app.js`, you'll find our `ThirdwebProvider` wrapping your app, this is necessary for our [hooks](https://portal.thirdweb.com/react) and
+On `src/index.jsx`, you'll find our `ThirdwebProvider` wrapping your app,
+this is necessary for our [hooks](https://portal.thirdweb.com/react) and
 [UI Components](https://portal.thirdweb.com/ui-components) to work.
 
 ### Deploy to IPFS
@@ -21,12 +22,13 @@ yarn deploy
 
 ## Learn More
 
-To learn more about thirdweb and Next.js, take a look at the following resources:
+To learn more about thirdweb, Vite and React, take a look at the following resources:
 
 - [thirdweb React Documentation](https://docs.thirdweb.com/react) - learn about our React SDK.
 - [thirdweb JavaScript Documentation](https://docs.thirdweb.com/react) - learn about our JavaScript/TypeScript SDK.
 - [thirdweb Portal](https://docs.thirdweb.com/react) - check our guides and development resources.
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
+- [Vite Documentation](https://vitejs.dev/guide/) - learn about Vite features.
+- [React documentation](https://reactjs.org/) - learn React.
 
 You can check out [the thirdweb GitHub organization](https://github.com/thirdweb-dev) - your feedback and contributions are welcome!
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,29 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
+import "./styles/Home.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+    <div className="container">
+      <main className="main">
+        <h1 className="title">
           Welcome to <a href="https://thirdweb.com/">thirdweb</a>!
         </h1>
 
-        <p className={styles.description}>
+        <p className="description">
           Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.js</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.js</code> file!
+          <code className="code">src/main.jsx</code>, then modify the{" "}
+          <code className="code">src/App.jsx</code> file!
         </p>
 
-        <div className={styles.connect}>
-          <ConnectWallet />
+        <div className="connect">
+          <ConnectWallet dropdownPosition={{
+            align: 'center',
+            side: 'bottom'
+          }} />
         </div>
 
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
+        <div className="grid">
+          <a href="https://portal.thirdweb.com/" className="card">
             <h2>Portal &rarr;</h2>
             <p>
               Guides, references and resources that will help you build with
@@ -28,7 +31,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
+          <a href="https://thirdweb.com/dashboard" className="card">
             <h2>Dashboard &rarr;</h2>
             <p>
               Deploy, configure and manage your smart contracts from the
@@ -36,10 +39,7 @@ export default function Home() {
             </p>
           </a>
 
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
+          <a href="https://portal.thirdweb.com/templates" className="card">
             <h2>Templates &rarr;</h2>
             <p>
               Discover and clone template projects showcasing thirdweb features.

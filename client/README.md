f2b1faaa09825e3f8779be4d04a6154680176ed2
Jeb <commit@jeb4.dev>
2023-05-15 19:33:11 +0300

    base setup
    Co-authored-by: Aminos24 <105971064+aminos24@users.noreply.github.com>

--- client/README.md
+++ client/README.md
@@ -8,7 +8,7 @@ npx thirdweb create --template vite-javascript-starter
 
 You can start editing the page by modifying `src/main.jsx`. The page auto-updates as you edit the file.
 
-On `src/index.jsx`, you'll find our `ThirdwebProvider` wrapping your app,
+On `src/main.jsx`, you'll find our `ThirdwebProvider` wrapping your app,
 this is necessary for our [hooks](https://portal.thirdweb.com/react) and
 [UI Components](https://portal.thirdweb.com/ui-components) to work.
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThirdwebProvider } from '@thirdweb-dev/react';
+import App from "./App.jsx";
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
+    <ThirdwebProvider chainId={11155111}>
+        <Router>
+            <App/>
+        </Router>
     </ThirdwebProvider>
-  </React.StrictMode>
-);
+)

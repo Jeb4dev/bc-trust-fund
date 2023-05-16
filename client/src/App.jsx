b0d9469f8d23f1105b405df64563d643b08621c6
Jeb <commit@jeb4.dev>
2023-05-16 16:23:10 +0300

    nav- and sidebar
    Co-authored-by: Aminos24 <105971064+aminos24@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,10 @@ const App = () => {
                 <Navbar/>
 
                 <Routes>
-                    <Route path={"/"} element={<Home/>}/>
+                    <Route path={"/"} element={<Home />} />
+                    <Route path={"/profile"} element={<Profile />} />
+                    <Route path={"/create-campaign"} element={<CreateCampaign />} />
+                    <Route path={"/campaign-details/:id"} element={<CampaignDetails />} />
                 </Routes>
 
             </div>

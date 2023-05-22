0f2b0c36825a1498431cb5d714c47664915f6a22
Jeb <commit@jeb4.dev>
2023-05-22 21:15:32 +0300

    show percentage of target

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -13,7 +13,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
             <div className="flex flex-col p-4">
                 <div className="flex flex-row items-center mb-[18px]">
                     <img src={tagType} alt="tag" className="w-[17px] h-[17px] object-contain"/>
-                    <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Education</p>
+                    <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Category</p>
                 </div>
 
                 <div className="block">
@@ -23,8 +23,8 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
 
                 <div className="flex justify-between flex-wrap mt-[15px] gap-2">
                     <div className="flex flex-col">
-                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{amountCollected}</h4>
-                        <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">Raised of {target}</p>
+                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{amountCollected / target * 100} %</h4>
+                        <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">{amountCollected} raised of {target}</p>
                     </div>
                     <div className="flex flex-col">
                         <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{remainingDays}</h4>

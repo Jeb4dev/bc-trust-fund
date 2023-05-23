0f5289d51f79dc95e9461356e7b9393ad6ff8108
Jeb <commit@jeb4.dev>
2023-05-23 15:01:59 +0300

    round goal percentage to two decimals

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -23,7 +23,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
 
                 <div className="flex justify-between flex-wrap mt-[15px] gap-2">
                     <div className="flex flex-col">
-                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{amountCollected / target * 100} %</h4>
+                        <h4 className="font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]">{(amountCollected / target * 100).toFixed(2)} %</h4>
                         <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate">{amountCollected} raised of {target}</p>
                     </div>
                     <div className="flex flex-col">
